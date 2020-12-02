eaee809760c3a07afcfb8af8d8361d940ad7ef3b
Ryan Gannon <ryan.gannon@trinetx.com>
2020-12-01 19:05:52 -0500

    Day 1 Part 1, post-cleanup

--- advent.lisp
+++ advent.lisp
@@ -4,17 +4,20 @@
 
 (defvar *day-1-input* "~/other/Lisp/advent/input/day1.txt")
 
+(defun ints-from-file (file)
+  (mapcar #'parse-integer (uiop:read-file-lines file)))
+
 ;; Open day1.txt, find the two numbers whose sum is 2020, and multiply them.
 (defun day-1-part-1 ()
-  (let ((nums (mapcar #'parse-integer (uiop:read-file-lines *day-1-input*)))
+  (let ((nums (ints-from-file *day-1-input*))
         (target 2020))
-    (labels ((find-sum (lst)
-               (cond
-                 ((or (atom lst) (null lst)) nil)
-                 (t (let* ((cur (first lst))
-                           (query (- target cur))
-                           (others (rest lst)))
-                      (if (member query others)
-                          (* cur query)
-                          (find-sum others)))))))
-      (find-sum nums))))
+    (labels ((find-sum (lst target)
+               (when (listp lst)
+                 (let* ((current (first lst))
+                        (query (- target current))
+                        (others (rest lst)))
+                   (if (member query others)
+                       (* current query)
+                       (find-sum others target))))))
+      (find-sum nums target))))
+
