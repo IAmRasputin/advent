e2fa296cca118e12645b17919068825376995cea
Ryan Gannon <ryanmgannon@gmail.com>
2020-12-15 22:15:51 -0500

    With memoization, this runs instantly instead of for several months

--- day10.lisp
+++ day10.lisp
@@ -9,6 +9,9 @@
 
 (load-input *input*)
 
+(defun device-rating ()
+  (+ 3 (apply #'max *adapters*)))
+
 (defun scan-for-differences (adapters)
   (let ((ones 1) ;; Port -> first adapter
         (threes 1)) ;; Last adapter -> device
@@ -26,8 +29,32 @@
         (+ 2 n)
         (+ 3 n)))
 
+(defun adapters-from (val)
+  (nthcdr (position val *adapters*) *adapters*))
 
 (defun part-1 ()
   (let ((diffs (scan-for-differences *adapters*)))
     (* (car diffs) (cdr diffs))))
+
+(defparameter *path-cache* (make-hash-table :test #'equal))
+
+;; oof
+(defun part-2 ()
+  (labels ((paths-from (adapters)
+             (let ((memo (gethash adapters *path-cache*)))
+               (if memo
+                   memo
+                   (let ((paths (apply #'+ (let* ((cur (car adapters))
+                                                  (next (valid-next cur)))
+                                             (mapcar (lambda (a)
+                                                       (cond
+                                                         ((= (device-rating) a) 1)
+                                                         ((member a *adapters*) (paths-from (adapters-from a)))
+                                                         (t 0)))
+                                                     next)))))
+                     (setf (gethash adapters *path-cache*) paths))))))
+    (paths-from (cons 0 *adapters*))))
+
+
+
 
