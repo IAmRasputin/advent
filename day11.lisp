706a73fa5fe0bdd54b2aad61f67d330c4f795ec6
Ryan Gannon <ryanmgannon@gmail.com>
2020-12-21 04:36:43 -0500

    Day 11, part 1

--- day11.lisp
+++ day11.lisp
@@ -11,22 +11,29 @@
     (#\# 'taken)))
 
 (defun parse-line (linestring)
-  (make-array (length linestring)
-              :initial-contents (mapcar #'parse-spot (coerce linestring 'list))))
+  (mapcar #'parse-spot (coerce linestring 'list)))
 
 (defun load-seat-map (file)
-  (setf *seat-map* (let ((lines (read-lines file)))
-                     (make-array (list (length lines) (length (car lines)))
-                                 :initial-contents (mapcar #'parse-line lines)))))
+  (setf *seat-map* (mapcar #'parse-line (read-lines file))))
 
 (load-seat-map *input*)
 
-(defun seat (row col)
-  (handler-case (aref *seat-map* row col)
-    (sb-int:invalid-array-index-error (e) (declare (ignore e)) nil)))
+(defun seat (seat-map row col)
+  (handler-case (nth col (nth row seat-map))
+    (type-error (e) (declare (ignore e))
+                           nil)))
 
-(defun adjacent-to (row col)
+
+(defun adjacent-to (seat-map row col)
   (remove-if #'null (mapcar #'seat 
+                            (list seat-map
+                                  seat-map
+                                  seat-map
+                                  seat-map
+                                  seat-map
+                                  seat-map
+                                  seat-map
+                                  seat-map)
                             (list (1+ row)
                                   (1+ row)
                                   (1+ row)
@@ -44,32 +51,38 @@
                                   col
                                   (- col 1)))))
 
-(defun next-spot (row col)
-  (let ((current (aref *seat-map* row col)))
+(defun next-spot-1 (seat-map row col)
+  (let ((current (seat seat-map row col)))
     (cond
+      ;; No adjacent seats taken -> occupied
       ((and (eq current 'empty)
             (every (lambda (s) (or
                                  (eq s 'empty)
                                  (eq s 'floor)))
-                   (adjacent-to row col)))
+                   (adjacent-to seat-map row col)))
        'taken)
-
+      ;; Four adjacent seats taken -> empty
       ((and (eq current 'taken)
-            (<= 4 (count 'taken (adjacent-to row col))))
+            (<= 4 (count 'taken (adjacent-to seat-map row col))))
        'empty)
-
-      ((eq current 'floor) 'floor)
-
+      ;; Else, unchanged
       (t current))))
 
+(defun next-1 (seat-map)
+  (loop for row below (length seat-map) collect
+        (loop for col below (length (car seat-map)) collect (next-spot-1 seat-map row col))))
 
-(defun next (seat-map)
-  (let* ((dimensions (array-dimensions seat-map))
-         (rows (car dimensions))
-         (cols (cadr dimensions))
-         (next (make-array dimensions)))
-    (loop for row below rows do
-          (loop for col below cols do
-                (setf (aref next row col) (next-spot row col))))
-    next))
+(defun seats-taken (seat-map)
+  (apply #'+ (mapcar (lambda (line)
+                       (count 'taken line))
+                     seat-map)))
+
+(defun stabilize-1 (seat-map)
+  (do* ((current seat-map after)
+        (after (next current) (next after)))
+      ((equal current after) (seats-taken current))))
+
+(defun part-1 ()
+  (stabilize *seat-map*))
+
 
