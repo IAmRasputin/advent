1603913e637bf1a4b681e01034602878d4559974
Ryan Gannon <ryanmgannon.dev@gmail.com>
2024-11-19 22:22:21 -0500

    headers and p elements

--- prompt.lisp
+++ prompt.lisp
@@ -4,8 +4,17 @@
   ;; TODO: Figure out how caching should work if the prompt updates after you
   ;; solve the first puzzle
   (let* ((pathstring (format nil "~d/~d" year day))
-         (dom (lquery:$ (initialize (api:aoc-get-prompt year day)))))
-    dom))
+         (dom (lquery:$ (initialize (api:aoc-get-prompt year day))))
+         (parts (lquery:$ dom "main article")))
+    (declare (ignorable pathstring)) ;; At least while I'm figuring out caching
+    (loop for part across parts do
+          (labels ((get-strings (element-or-class)
+                     (lquery:$ part element-or-class (text))))
+            (let* ((header (aref (get-strings "h2") 0))
+                   (words (get-strings "p")))
+              (write-line header)
+              (loop for p across words do
+                    (format t "~s~%" p)))))))
 
 (defun prompt/handler (cmd)
   (let* ((year (clingon:getopt cmd :year))
@@ -19,5 +28,3 @@
     :name "prompt"
     :description "get the prompt for a day's puzzle"
     :handler #'prompt/handler))
-
-
